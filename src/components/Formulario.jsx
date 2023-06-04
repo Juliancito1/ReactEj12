5e49da2e09b5a5ebe0446d19f943192b6bdd5e87
Julian <julianser@hotmail.com.ar>
2023-06-04 00:58:44 -0300

    Se agrego que se puedan ver noticias por lenguaje ademas de la categoria

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,18 +5,19 @@ import ListaNoticias from "./ListaNoticias";
 const Formulario = () => {
     const[noticias , setNoticias] = useState([])
     const[categoria, setCategoria] = useState('');
+    const[idioma, setIdioma] = useState('')
     useEffect(() => {
         if (noticias.length === 0) {
             consultarAPI();
           }
           setNoticias([])
-    },[categoria])
+    },[categoria, idioma])
 
 
     const consultarAPI = async () => {
         try {
-            if(categoria !== ""){
-                const respuesta = await fetch(`https://newsdata.io/api/1/news?apikey=pub_2386188cc0b2582136aefe10a1efcae0a03eb&category=${categoria}`)
+            if(categoria !== "" &&  idioma !== ""){
+                const respuesta = await fetch(`https://newsdata.io/api/1/news?apikey=pub_2386188cc0b2582136aefe10a1efcae0a03eb&category=${categoria}&language=${idioma}`)
                 const dato = await respuesta.json()
                 setNoticias(...noticias,dato.results)
                 }
@@ -28,7 +29,7 @@ const Formulario = () => {
     return (
         <Container className="my-5 border border-3 border-black">
             <Form>
-            <Form.Group className="my-3 justify-content-center d-flex border-bottom border-black border-1" controlId="formCategoria">
+            <Form.Group className="my-3 justify-content-center d-flex" controlId="formCategoria">
             <Form.Label className="d-inline fs-4">Buscar por Categoria: </Form.Label>
             <Form.Select className="d-inline w-50 ms-3 mb-4" aria-label="Categoria" onChange={(e) => setCategoria(e.target.value)} value={categoria}>
             <option value=''>Opciones</option>
@@ -36,6 +37,15 @@ const Formulario = () => {
             <option value="top">Top</option>
             <option value="business">Negocios</option>
             <option value="technology">Tecnologia</option>
+            </Form.Select>
+            </Form.Group>
+            <Form.Group className="my-3 justify-content-center d-flex border-bottom border-black border-1" controlId="formIdioma">
+            <Form.Label className="d-inline fs-4">Seleccionar Idioma: </Form.Label>
+            <Form.Select className="d-inline w-50 ms-3 mb-4" aria-label="Idioma" onChange={(e) => setIdioma(e.target.value)} value={idioma}>
+            <option value=''>Opciones</option>
+            <option value="es">Español</option>
+            <option value="en">Ingles</option>
+            <option value="pt">Portugues</option>
             </Form.Select>
             </Form.Group>
             </Form>
